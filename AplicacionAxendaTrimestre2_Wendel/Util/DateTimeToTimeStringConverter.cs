80a8f75856a2cd6b0b2ae0fce02a79ef68c66fb8
wendellchabaud <wendelledgardossantoschabaud@gmail.com>
2024-03-12 13:51:29 +0100

    agregado reloj syncfusion

---
+++ AplicacionAxendaTrimestre2_Wendel/Util/DateTimeToTimeStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace AplicacionAxendaTrimestre2_Wendel.Util
+{
+
+        public class DateTimeToTimeStringConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                if (value is DateTime dateTime)
+                {
+                    return dateTime.ToString("HH:mm:ss");
+                }
+                return string.Empty;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    
+}
