be2aa48a3887b04e20c951907a5d9609d9805ed7
Wendell Edgar Dos Santos Chabaud <wendelledgardossantoschabaud@gmail.com>
2024-03-14 01:08:15 +0100

    Agregando registro Evento

---
+++ AplicacionAxendaTrimestre2_Wendel/UI/Views/Registros/RegEvento.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AplicacionAxendaTrimestre2_Wendel.UI.Views.Registros
+{
+    /// <summary>
+    /// Lógica de interacción para RegEvento.xaml
+    /// </summary>
+    public partial class RegEvento : Page
+    {
+        public RegEvento()
+        {
+            InitializeComponent();
+        }
+    }
+}
